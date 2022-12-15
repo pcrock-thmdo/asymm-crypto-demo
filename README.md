921ec23bba12a50af82f694ba13c6b6bdf5591b9
Philip Crockett <philip.crockett@thermondo.de>
2022-12-15 19:38:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Asymmetric Cryptography Demo
+
+Work in progress.
+
+Demonstrates how asymmetric cryptography works using basic CLI tools.
+
+## Getting Started
+
+### Install dependencies
+
+Make sure [age](https://github.com/FiloSottile/age) is installed:
+
+* macOS: `brew install age`
+* Ubuntu: `sudo apt update && sudo apt install age`
+
+### Set up your repo
+
+Copy [.env.example](.env.example) to `.env` and fill it out with your information.
+
+Do the same for [allowed-signers.example](allowed-signers.example).
+
---
+++ encrypt.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+panic() {
+    echo "${1}"
+    exit 1
+}
+
+test "${#}" -ge 1 || panic "Usage: encrypt.sh [RECIPIENT_PUB_KEY_PATH]"
+
+readonly RECIPIENT_PUB_KEY_PATH="${1}"
+
+test -f .env || panic "You haven't created your .env file yet"
+
+source .env
+
+rm -Rf .temp
+mkdir -p .temp
+
+cat > .temp/plaintext
+age --recipients-file "${RECIPIENT_PUB_KEY_PATH}" --armor > .temp/ciphertext < .temp/plaintext
+ssh-keygen -Y sign -f "${USER_PRIVATE_KEY}" -n demo > .temp/plaintext.sig < .temp/plaintext
+
+echo "
+Encrypted message:
+
+$(cat .temp/ciphertext)
+
+Signature:
+
+$(cat .temp/plaintext.sig)
+"
+
+rm .temp/plaintext
