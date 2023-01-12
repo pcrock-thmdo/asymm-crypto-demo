ad6082daf1d72b0e9be2b24649c5d9e56feb2357
Philip Crockett <philip.crockett@thermondo.de>
2023-01-12 16:25:24 +0100

    Explanatory comments for decrypt_and_verify.sh

--- decrypt_and_verify.sh
+++ decrypt_and_verify.sh
@@ -12,7 +12,9 @@ test "${#}" -ge 1 || panic "Usage: ./decrypt_and_verify.sh [SENDER_EMAIL_ADDRESS
 readonly SENDER_EMAIL_ADDRESS="${1}"
 
 test -f .env || panic "You haven't created your .env file yet"
+test -f allowed-signers || panic "You haven't created your allowed-signers file yet"
 
+# Read settings in .env file
 # shellcheck disable=1091
 source .env
 
@@ -20,11 +22,13 @@ source .env
 rm -Rf .temp
 mkdir -p .temp
 
+# Save ciphertext in a file
 echo "
 Paste CIPHERTEXT and press Ctrl + D when finished:
 "
 cat > .temp/ciphertext
 
+# Decrypt ciphertext, saving the result in a plaintext file
 age --identity "${USER_PRIVATE_KEY}" \
     --decrypt \
     --output .temp/plaintext \
@@ -36,11 +40,13 @@ Decrypted message:
 $(cat .temp/plaintext)
 "
 
+# Save signature in a .sig file
 echo "
 Paste SIGNATURE and press Ctrl + D when finished:
 "
 cat > .temp/plaintext.sig
 
+# Verify signature for plaintext
 ssh-keygen -Y verify \
     -f "allowed-signers" \
     -I "${SENDER_EMAIL_ADDRESS}" \
